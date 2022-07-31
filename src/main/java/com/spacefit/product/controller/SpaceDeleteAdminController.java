a026a98282cf6b44e27a08228b2bcb8d4107ca93
yoonahyoung <97347193+yoonahyoung@users.noreply.github.com>
2022-07-31 23:05:56 +0900

    feat : SpcaeDeleteAdminController.java추가
    author : ahyoungyoon

---
+++ src/main/java/com/spacefit/product/controller/SpaceDeleteAdminController.java
@@ -0,0 +1,53 @@
+package com.spacefit.product.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.spacefit.product.model.service.SpaceService;
+
+/**
+ * Servlet implementation class SpaceDeleteAdminController
+ */
+@WebServlet("/adDelete.sp")
+public class SpaceDeleteAdminController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SpaceDeleteAdminController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int spNo = Integer.parseInt(request.getParameter("no"));
+		
+		int result = new SpaceService().deleteSpace(spNo);
+		
+		if(result > 0) {
+			request.getSession().setAttribute("alertMsg", "성공적으로 삭제되었습니다.");
+			response.sendRedirect(request.getContextPath()+ "/adList.sp");
+		}else {
+			request.setAttribute("errorMsg", "삭제에 실패하였습니다.");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
