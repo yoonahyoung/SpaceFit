8c20f209229aa901e39ec51ad28111065a206855
yoonahyoung <97347193+yoonahyoung@users.noreply.github.com>
2022-08-02 11:03:21 +0900

    feat : 장바구니 기능 vo 추가
    author : ahyoungyoon

---
+++ src/main/java/com/spacefit/mem/model/vo/Cart.java
@@ -0,0 +1,131 @@
+package com.spacefit.mem.model.vo;
+
+public class Cart {
+	private int memNo;
+	private int spaceNo;
+	private int cartLimit;
+	private String cartDate;
+	private String	cartIn;
+	private String cartOut;
+	private String cartParking;
+	private String cartAnimal;
+	private String cartStand;
+	private String cartChair;
+	private int cartPrice;
+	
+	public Cart() {}
+
+	public Cart(int memNo, int spaceNo, int cartLimit, String cartDate, String cartIn, String cartOut,
+			String cartParking, String cartAnimal, String cartStand, String cartChair, int cartPrice) {
+		super();
+		this.memNo = memNo;
+		this.spaceNo = spaceNo;
+		this.cartLimit = cartLimit;
+		this.cartDate = cartDate;
+		this.cartIn = cartIn;
+		this.cartOut = cartOut;
+		this.cartParking = cartParking;
+		this.cartAnimal = cartAnimal;
+		this.cartStand = cartStand;
+		this.cartChair = cartChair;
+		this.cartPrice = cartPrice;
+	}
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+	public int getSpaceNo() {
+		return spaceNo;
+	}
+
+	public void setSpaceNo(int spaceNo) {
+		this.spaceNo = spaceNo;
+	}
+
+	public int getCartLimit() {
+		return cartLimit;
+	}
+
+	public void setCartLimit(int cartLimit) {
+		this.cartLimit = cartLimit;
+	}
+
+	public String getCartDate() {
+		return cartDate;
+	}
+
+	public void setCartDate(String cartDate) {
+		this.cartDate = cartDate;
+	}
+
+	public String getCartIn() {
+		return cartIn;
+	}
+
+	public void setCartIn(String cartIn) {
+		this.cartIn = cartIn;
+	}
+
+	public String getCartOut() {
+		return cartOut;
+	}
+
+	public void setCartOut(String cartOut) {
+		this.cartOut = cartOut;
+	}
+
+	public String getCartParking() {
+		return cartParking;
+	}
+
+	public void setCartParking(String cartParking) {
+		this.cartParking = cartParking;
+	}
+
+	public String getCartAnimal() {
+		return cartAnimal;
+	}
+
+	public void setCartAnimal(String cartAnimal) {
+		this.cartAnimal = cartAnimal;
+	}
+
+	public String getCartStand() {
+		return cartStand;
+	}
+
+	public void setCartStand(String cartStand) {
+		this.cartStand = cartStand;
+	}
+
+	public String getCartChair() {
+		return cartChair;
+	}
+
+	public void setCartChair(String cartChair) {
+		this.cartChair = cartChair;
+	}
+
+	public int getCartPrice() {
+		return cartPrice;
+	}
+
+	public void setCartPrice(int cartPrice) {
+		this.cartPrice = cartPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [memNo=" + memNo + ", spaceNo=" + spaceNo + ", cartLimit=" + cartLimit + ", cartDate=" + cartDate
+				+ ", cartIn=" + cartIn + ", cartOut=" + cartOut + ", cartParking=" + cartParking + ", cartAnimal="
+				+ cartAnimal + ", cartStand=" + cartStand + ", cartChair=" + cartChair + ", cartPrice=" + cartPrice
+				+ "]";
+	}
+
+	
+}
